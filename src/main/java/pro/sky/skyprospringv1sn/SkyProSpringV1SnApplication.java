72d134890c47027fc235612064eb9ed93bc65f39
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-09-16 21:58:57 +0400

    v1

---
+++ src/main/java/pro/sky/skyprospringv1sn/SkyProSpringV1SnApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringv1sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSpringV1SnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProSpringV1SnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringv1sn/SkyProSpringV1SnApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringv1sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringV1SnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
